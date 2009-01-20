fb7ddb968b13743d859a4e8eac03884f4b23cfbe
Will Riley <will@psychdesigns.net>
2009-01-20 16:23:00 -0500

    Worked on #254 a bit, still not finished though

--- backend/lib/session.php
+++ backend/lib/session.php
@@ -1,9 +1,18 @@
 <?php
 
 import("models.session");
-
-class session_manager {
-    var $session_id
+/*class session_manager {
+    var $session_id;
+    function _makeSession($session_id){
+        global $Session;
+        $p = new $Session(array(
+            "session_id" => $session_id,
+            "date_creatd" => getTimeStamp(),
+            "last_updated" => getTimeStamp(),
+        ));
+        $p->save();
+        return $p;
+    }
     function open($save_path, $session_name){
         //do nothing
         return true;
@@ -32,6 +41,36 @@ class session_manager {
         }
     }
     function write($session_id, $session_data){
-        
+        global $Session;
+        $p=$Session->filter("session_id", $session_id);
+        if($p != false){
+            $p = $p[0];
+            $p->last_updated = getTimeStamp();
+        }else{
+            $p = $this->_makeSession();
+        }
+        $p->session_data = $session_data;
+        $p->save();
+        return true;
     }
-}
+    function destroy($session_id){
+        global $Session
+        $p=$Session->filter("session_id", $session_id);
+        if($p != false){
+            $p = $p[0];
+            $p->delete();
+            return true;
+        }
+        return false;
+    }
+    function gc($max_lifetime){
+        global $Session;
+        $real_now = date('Y-m-d H:i:s');
+        $dt1 = strtotime("$real_now -2 hours");
+        $dt2 = date('YmdHis', $dt1);
+        
+        $count = $Session->filter(array("last_updated__lte" => $dt2));
+        return TRUE;
+    }
+}*/
+echo "asdf";
--- backend/lib/util.php
+++ backend/lib/util.php
@@ -49,6 +49,12 @@ function import($module) {
 		return false;
 	}
 }
+//for debugging
+function desktop_errorHandler($exception) {
+	internal_error("generic_err", $exception->getMessage());
+}
+set_exception_handler("desktop_errorHandler");
+
 //sessions and cookies
 function get_basepath() {
 	$curpath = explode("/", $_SERVER['REQUEST_URI']);
@@ -60,6 +66,15 @@ function get_basepath() {
 	array_pop($curpath);
 	return implode("/", $curpath) . "/";
 }
+/*
+import("lib.session");
+$session_class = new session_manager();
+session_set_save_handler(array(&$session_class, 'open'),
+                         array(&$session_class, 'close'),
+                         array(&$session_class, 'read'),
+                         array(&$session_class, 'write'),
+                         array(&$session_class, 'destroy'),
+                         array(&$session_class, 'gc'));*/
 $sesPath = $GLOBALS['path']."/../tmp/sessions/";
 if(!is_dir($sesPath)) mkdir($sesPath, 777);
 if(is_writable($sesPath)) session_save_path($sesPath);
@@ -72,11 +87,8 @@ ini_set("session.gc_maxlifetime",$time);
 session_start();
 
 
-//for debugging
-function desktop_errorHandler($exception) {
-	internal_error("generic_err", $exception->getMessage());
-}
-set_exception_handler("desktop_errorHandler");
+
+
 //test session token
 $omit_backends = array(
     "core.bootstrap.check.getToken",
