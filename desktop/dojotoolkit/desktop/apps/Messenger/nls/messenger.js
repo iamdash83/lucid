c807feaff16440068379013eb369454991847a95
root <root@puppypc.(none)>
2009-02-03 19:28:54 +0800

    made Messenger translatable (#238)

---
+++ desktop/dojotoolkit/desktop/apps/Messenger/nls/messenger.js
@@ -0,0 +1,9 @@
+({
+	"contactList": "Contact List",
+	"addUser": "Add User",
+	"removeUser": "Remove User",
+	"contacts": "Contacts",
+	"userOffline": "System: User is offline or is experiencing network difficulites. Message will be sent when user is back online.",
+	"noUser": "User specified does not exist!",
+	
+})
