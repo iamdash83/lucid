b3dae824ab991755cdefc93b32b47a43d5c1191c
Will Riley <will@psychdesigns.net>
2009-09-19 17:33:10 -0400

    worked on the header widget

--- dojotoolkit/lucid/widget/Header.js
+++ dojotoolkit/lucid/widget/Header.js
@@ -1,3 +1,12 @@
 dojo.provide("lucid.widget.Header");
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require("dijit._Contained");
+dojo.require("dijit.form.DropDownButton");
+dojo.require("dijit.Menu");
+dojo.require("dijit.TooltipDialog");
 
-
+dojo.declare("lucid.widget.Header", [dijit._Widget, dijit._Templated, dijit._Contained], {
+    templatePath: dojo.moduleUrl("lucid.widget", "templates/Header.html"),
+    widgetsInTemplate: true
+});
