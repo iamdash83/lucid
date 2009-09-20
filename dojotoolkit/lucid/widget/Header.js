aba4ff583ca5deda7cbdbdef68d3f5f822715745
Will Riley <will@psychdesigns.net>
2009-09-19 23:35:18 -0400

    fixed some stuff, made the label go through dojo.attr to make things smoother

--- dojotoolkit/lucid/widget/Header.js
+++ dojotoolkit/lucid/widget/Header.js
@@ -9,14 +9,25 @@ dojo.require("dijit.TooltipDialog");
 dojo.declare("lucid.widget.Header", [dijit._Widget, dijit._Templated, dijit._Contained], {
     templatePath: dojo.moduleUrl("lucid.widget", "templates/Header.html"),
     widgetsInTemplate: true,
+    userLabel: "Guest",
     postCreate: function() {
         dojo.subscribe("/user/login", dojo.hitch(this, function(data) {
-            alert('/user/login recieved by header');
-            this.userNode.innerHTML = data;
+            console.log('/user/login recieved by header');
+            this.attr("userLabel", data);
         }));
         dojo.subscribe("/user/logout", dojo.hitch(this, function(data) {
-            alert('/user/logout recieved by header');
-            this.userNode.innerHTML = "Guest";
+            console.log('/user/logout recieved by header');
+            this.attr("userLabel", false);
         }));  
+    },
+    _setUserLabelAttr: function(val){
+        if(val == false){
+            dojo.style(this.userDisplayNode, "display", "none");
+            dojo.style(this.guestDisplayNode, "display", "block");
+        }else{
+            dojo.style(this.userDisplayNode, "display", "block");
+            dojo.style(this.guestDisplayNode, "display", "none");
+            this.userNode[dojo.isIE ? "innerText" : "textContent"] = val;
+        }
     }
 });
