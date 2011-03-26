1e3b5d5acf5c4fb51e2b2de84b480bec4ebcf4a2
Will Riley <will@hflw.org>
2011-03-25 23:59:49 -0400

    added CSS transitions for aesthetic value™

--- dojotoolkit/lucid/widget/core/ContactsDropDown.js
+++ dojotoolkit/lucid/widget/core/ContactsDropDown.js
@@ -1,7 +1,7 @@
 define("lucid/widget/core/ContactsDropDown", ["dojo", "dijit", "lucid", "dijit/_Widget", "dijit/_Templated"], function(dojo, dijit, lucid){
     
     dojo.declare("lucid.widget.core.ContactsDropDown", [dijit._Widget, dijit._Templated], {
-        
+         
         templateString: dojo.cache("lucid.widget.core", "templates/DropDown.html"),
 
         postCreate: function(){
