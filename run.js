e7fd500811d573fa305ad44126f8df7cfaa27b33
Will Riley <will@hflw.org>
2011-10-01 23:00:17 -0400

    Added bootstrap code, so now this thing actually runs

--- run.js
+++ run.js
@@ -1,1 +1,6 @@
-var promise = require("promise/promise").Promise;
+var Lucid = require("./lib/Lucid");
+
+new Lucid({
+    config: process.argv[2] || "config/main.json",
+    rootPath: __dirname
+});
