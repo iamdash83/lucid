3a863339144eb114a93f8dfb14e037f2fb44373b
Will Riley <will@hflw.org>
2013-01-02 02:06:29 -0500

    added some more sample apps + containers for testing purposes

---
+++ packages/sample_app/test/main.js
@@ -0,0 +1,8 @@
+//var test = require("./test");
+//test.foo == "bar"
+var n = 1;
+setInterval(function(){
+    var foo = "bar";
+    console.error("sample_app (second container) is still running after "+(n*10)+" seconds!");
+    n++;
+}, 10000);
