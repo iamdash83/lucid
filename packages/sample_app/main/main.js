0001c232993acb3236c898b8c5e99afecf28742e
Will Riley <will@hflw.org>
2013-01-03 20:53:26 -0500

    Testing out some stuff with web requests

--- packages/sample_app/main/main.js
+++ packages/sample_app/main/main.js
@@ -4,16 +4,28 @@
 var n = 0;
 var started = new Date();
 
-rpc.expose("web", function(){
-    console.error("got a request!", arguments);
-    return "<h1>It Works!</h1>"
-         + "<p>This is a Lucid app responding to a web request!</p>"
-         + "<ul>"
-         + "<li>Run Time: "+n+" seconds</li>"
-         + "<li>PID: "+process.pid+"</li>"
-         + "<li>Time Started: "+started+" </li>"
-         + "<li>Time Now: "+new Date()+" </li>"
-         + "</ul>";
+rpc.expose("web", function(path){
+    var p = new Promise();
+
+    console.error("got a request!", path);
+
+    console.error((new Error("bla")).stack);
+    
+    if(path != "")
+        p.errback(new Error("404"));
+    else
+        p.callback(
+               "<h1>It Works!</h1>"
+             + "<p>This is a Lucid app responding to a web request!</p>"
+             + "<ul>"
+             + "<li>Run Time: "+n+" seconds</li>"
+             + "<li>PID: "+process.pid+"</li>"
+             + "<li>Time Started: "+started+" </li>"
+             + "<li>Time Now: "+new Date()+" </li>"
+             + "</ul>"
+        );
+
+    return p;
 });
 
 setInterval(function(){
